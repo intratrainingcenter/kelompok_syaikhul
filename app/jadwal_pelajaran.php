dbbdb484f95b47b1bca16c7e45f8dd5f65c635b7
Syaikhul <msasyaikhul@gmail.com>
2018-09-22 18:00:46 +0700

    create mapel

--- app/jadwal_pelajaran.php
+++ app/jadwal_pelajaran.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class jadwal_pelajaran extends Model
 {
-    //
+	protected $fillable = ['id_kelas','hari','kode_pelajaran','pelajaran'];
+
+    public function getkelas()
+  	{
+  		return $this->belongsTo('App\kelas','id_kelas','id');
+	}
 }
