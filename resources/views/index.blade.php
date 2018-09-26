f0feb89ad1dade756b01f656087885c90096bd95
Syaikhul <msasyaikhul@gmail.com>
2018-09-26 14:48:59 +0700

    hapus header salah

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -32,17 +32,6 @@
   </aside>
 
   <div class="content-wrapper">
-    <section class="content-header">
-      <h1>
-        Dashboard
-        <small>Control panel</small>
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Dashboard</li>
-      </ol>
-    </section>
-
     <section class="content">
       @yield('content')
     </section>
